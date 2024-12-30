da64357bb9641bfae8407ed309dc3cea22c0c990
Pybeel <pyqhansen@gmail.com>
2024-12-30 19:58:28 +0100

    Nav bar ændringer

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ function hideSidebar () {
     const sidebar = document.querySelector ('.sidebar')
     sidebar.style.display = 'none'
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dropdownLink = document.getElementById('dropdownLink');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+
+
+dropdownLink.addEventListener('click', (e) => {
+    e.preventDefault(); // Prevent default link behavior
+     const isVisible = dropdownMenu.style.display === 'block';
+     dropdownMenu.style.display = isVisible ? 'none' : 'block';
+});
+
+   
+document.addEventListener('click', (e) => {
+if (!dropdownMenu.contains(e.target) && !dropdownLink.contains(e.target)) {
+            dropdownMenu.style.display = 'none';
+    }
+});
+});
+
