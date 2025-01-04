48c4b00832d1b71439596fb3defde4c8d8171c76
Pybeel <pyqhansen@gmail.com>
2025-01-04 01:40:16 +0100

    sidste sktion tilføjet (testimonials og logoer)

--- js/script.js
+++ js/script.js
@@ -85,3 +85,32 @@ document.addEventListener('DOMContentLoaded', () => {
     startCarousel();
 });
 
+var swiper = new Swiper(".custom-slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      950: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
